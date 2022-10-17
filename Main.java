1963c44cd3d729a658e9d089c9af4fdaea665b1b
EwelinaPodleseicka <92682631+EwelinaPodleseicka@users.noreply.github.com>
2022-10-17 22:41:15 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -86,4 +86,64 @@ import java.util.Scanner;
         }
 }
 
+package org.example;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj a:");
+        int a, b, c;
+        a = scanner.nextInt();
+        System.out.println("Podaj b:");
+        b = scanner.nextInt();
+
+        Main main = new Main();
+        int liczba1 = main.losuj(a, b);
+        System.out.println("Liczba: " + liczba1);
+        System.out.println("Czy parzysta: " + main.czyParzysta(liczba1));
+        int liczba2 = main.losuj(a, b);
+        System.out.println("Liczba: " + liczba2);
+        System.out.println("Czy podzielna przez 3 i 5: " + main.czyPodzielna(liczba2));
+        int liczba3 = main.losuj(a, b);
+        System.out.println("Liczba: " + liczba3);
+        System.out.println("Trzecia potęga: " + main.doTrzeciej(liczba3));
+        int liczba4 = main.losuj(a, b);
+        System.out.println("Liczba: " + liczba4);
+        System.out.println("Pierwiastek: " + main.pierwiastek(liczba4));
+        int liczba5 = main.losuj(a, b);
+        int liczba6 = main.losuj(a, b);
+        int liczba7 = main.losuj(a, b);
+        System.out.println("Wylosowane liczby: " + liczba5 + ", " + liczba6 + ", " + liczba7);
+        System.out.println("Czy można zbudować trójkąt prostokątny: " + main.czyTrojkatProstokatny(liczba5, liczba6, liczba7));
+    }
+
+    public boolean czyParzysta(int liczba) {
+        return liczba % 2 == 0;
+    }
+
+    public boolean czyPodzielna(int liczba) {
+        return liczba % 3 == 0 && liczba % 5 == 0;
+    }
+
+    public int doTrzeciej(int liczba) {
+        return liczba * liczba * liczba;
+    }
+
+    public double pierwiastek(int liczba) {
+        return Math.sqrt(liczba);
+    }
+
+    public boolean czyTrojkatProstokatny(int a, int b, int c) {
+        return (a * a + b * b == c * c) || (a * a + c * c == b * b) || (c * c + b * b == a * a);
+    }
+    
+    private int losuj(int a, int b) {
+        Random r = new Random();
+        return r.nextInt(b - a) + a;
+    }
+}
+
 
