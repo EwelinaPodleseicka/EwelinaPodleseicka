b403352b4ca626d2b29d988732a0fddc35115760
EwelinaPodleseicka <92682631+EwelinaPodleseicka@users.noreply.github.com>
2022-10-18 09:30:19 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -146,4 +146,129 @@ public class Main {
     }
 }
 
+/*import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+    }
+    //zad1
+    public static void zadanie1() {
+        int n, a=0, b=10;
+        double suma=0, pkt, ile=0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Podaj ilosc studentow");
+        n=input.nextInto();
+        if(n>0){
+            while(n>0){
+                System.out.println("podaj ilosc pkt: 0-10");
+                pkt= input.nextDouble();
+                if(pkt>=a && pkt<=b){
+                    suma+=pkt;
+                    ile++;
+                    n--;
+                }
+            }// konec while
+        System.out.println("ile:" +ile);
+            System.out.println("suma :" +suma);
+            System.out.println("srednia: " +(suma/ile));
+        }
+        else{
+            System.out.println()
+        }
+    }
+}
+ */
+import java.util.Locale;
+//import java.util.Scanner;
+/*
+public class Main {
+    public static void main(String[] args){
+        zadanie2();
+    }
+    public static void zadanie2(){
+        int ile_d=0, ile_u=0;
+        double sumaD=0, sumaU=0, liczba;
+        for (int i = 0; i < 10  ; i++) {
+            liczba= Input();
+            if(liczba >0){
+                ile_d++, sumaD+=liczba;
+            }else {
+                ile_u++; sumaU+=liczba;
+            }
+            System.out.println("Ilosc liczb dodatnich " +ile_d," suma" = sumaD);
+            System.out.println("Ilosc liczb ujemnych " +ile_u," suma" = sumaU);
+        }
+    }
+    private static double Input() {
+    }
+}
+//*
+*/
 
+/*public class Main {
+    public static void main(String[] args){
+        zadanie3();
+    }
+    private static void zadanie3() {
+    }
+    //zad3
+    public static double Input() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Podaj ilosc studentow");
+        double n = input.nextDouble();
+        return n;
+    }
+}
+ */
+ /* zadanie 4
+public class Main {
+    public static boolean czyPalindorm(String s){
+        int l = s.length() -1;
+        s.toLowerCase(Locale.ROOT);
+        for (int i = 0; i < s.length()/2; i++) {
+            if(s.charAt(i) !=s.charAt(l)) return false;
+            l--;
+        }
+        return true;
+    }
+}
+  */
+
+
+import static java.lang.Math.sqrt;
+        import java.util.Scanner;
+public class Kwadratowe {
+    public static void main(String[] args) {
+        double a, b, c;
+        Scanner klaw = new Scanner(System.in);
+        System.out.print("a=");
+        a = klaw.nextFloat();
+        System.out.print("b=");
+        b = klaw.nextFloat();
+        System.out.print("c=");
+        c = klaw.nextFloat();
+        if (a == 0)
+            liniowe(b, c);
+        else {
+            double delta;
+            delta = b * b - 4 * a * c;
+            if (delta < 0)
+                System.out.println("Brak rozwiązań");
+            else if (delta == 0) {
+                double x;
+                x = -b / (2 * a);
+                System.out.print("Jedno podwójne rozwiązanie x = ");
+                System.out.println(x);
+            } else {
+                double x1, x2;
+                x1 = (-b + sqrt(delta)) / (2 * a);
+                x2 = (-b - sqrt(delta)) / (2 * a);
+                System.out.print("x1 = ");
+                System.out.println(x1);
+                System.out.print("x2 = ");
+                System.out.println(x2);
+            }
+        }
+    }
+
+   private static void liniowe(double b, double c) {
+    };
